89bd9b8077b8696783903ad2a53df9879bc89e14
limar <you@example.com>
2022-11-16 10:53:58 -0300

    16/11

---
+++ projeto_a3/projeto_a3_htmlcssfinal/telas/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './views/cadastro/cadastro.component';
+import { LoginComponent } from './views/login/login.component';
+import { InicialComponent } from './views/inicial/inicial.component';
+import { ProntuarioComponent } from './views/prontuario/prontuario.component';
+import { ListagemComponent } from './views/listagem/listagem.component';
+
+const routes: Routes = [
+  {path: '', component: InicialComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'listagem', component:ListagemComponent},
+  {path: 'prontuario', component: ProntuarioComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
